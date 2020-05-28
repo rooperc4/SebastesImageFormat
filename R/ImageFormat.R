3f0c7dec18dde62c643606e1c9a02f44e8c4b044
Chris Rooper <chris.rooper@dfo-mpo.gc.ca>
2020-05-28 08:45:17 -0700

    Fixed date format issue in .db3 file

--- R/ImageFormat.R
+++ R/ImageFormat.R
@@ -92,19 +92,19 @@ ImageFormat<-function(pathFrom=NA,pathTo=NA){
     image_write(frinkR, path = paste(pathTo,"/images/WeeViewRight/",formatC(i, width = 4, format = "d", flag = "0"),"_Right.jpg",sep=""), format = "jpg")
     images[k,1]<-i
     images[k,2]<-cameras$camera[cameras$label=="right"]
-    images[k,3]<-Sys.time()
+    images[k,3]<-as.character(Sys.time())
     images[k,4]<-paste(formatC(i, width = 4, format = "d", flag = "0"),"_Right.jpg",sep="")
     images[k,5]<-2500
     k<-k+1
     image_write(frinkL, path = paste(pathTo,"/images/WeeViewLeft/",formatC(i, width = 4, format = "d", flag = "0"),"_Left.jpg",sep=""), format = "jpg")
     images[k,1]<-i
     images[k,2]<-cameras$camera[cameras$label=="left"]
-    images[k,3]<-Sys.time()
+    images[k,3]<-as.character(Sys.time())
     images[k,4]<-paste(formatC(i, width = 4, format = "d", flag = "0"),"_Left.jpg",sep="")
     images[k,5]<-2500
     k<-k+1
 
-    async_data[i,1]<-Sys.time()
+    async_data[i,1]<-as.character(Sys.time())
     async_data[i,2]<-"Camera"
     async_data[i,3]<-"$CTCS"
     async_data[i,4]<-"$CTCS,WeeViewCamera"
